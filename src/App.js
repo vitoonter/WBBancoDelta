7dde66bd835a4bc64232fe73c5e61cb0363db5f0
william <wcubasalas@gmail.com>
2016-09-15 09:11:13 -0500

    delete favicon add images add atomic component footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './base.css';
+import './modules.css';
+import Footer from './components/atoms/footer';
 
 class App extends Component {
   render() {
     return (
-        <div></div>
+        <div>
+            <Footer></Footer>
+        </div>
     );
   }
 }
