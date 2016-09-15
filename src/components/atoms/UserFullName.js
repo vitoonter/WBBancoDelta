465da976c0b9e7176a3301f2d39f305360436797
Jhoel <jhoel.quispe.123@gmail.com>
2016-09-15 14:00:56 -0500

    user atoms: name, image, lastconnection

---
+++ src/components/atoms/UserFullName.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const UserFullName = ({text}) => (
+  <div className="username">
+    {text}
+  </div>
+)
+
+export default UserFullName;
---
+++ src/components/atoms/UserImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import userImage from '../../images/userImage.png';
+
+const UserImage = () => (
+  <a href="#" className="user-image l-right hidden-xs">
+    <img src={userImage}></img>
+  </a>
+)
+
+export default UserImage;
