34d1849dc7058d6f0d7d94c6838cb4faa387da9e
Jhoel <jhoel.quispe.123@gmail.com>
2016-09-16 10:18:19 -0500

    header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './base.css';
+import './layout.css'
 import './modules.css';
+import Header from './components/templates/container/Header';
 import Footer from './components/templates/container/Footer';
 
 const App = ({ children }) => 
     <div>
     	{ children }
-        <Footer></Footer>
+        <Header />
+        <Footer />
     </div>
 
 export default App;
