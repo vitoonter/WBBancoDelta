0aa1d06f40baa6f005eb7b0d776b3ad9033e8cd3
william <wcubasalas@gmail.com>
2016-09-16 09:58:05 -0500

    add steps component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './base.css';
 import './layout.css'
 import './modules.css';
+import './state.css';
 import Header from './components/templates/container/Header';
 import Footer from './components/templates/container/Footer';
 import LoginUser from './components/templates/login/LoginUser'
 import LoginPassword from './components/templates/login/LoginPassword'
 
-const App = ({ children }) => 
+const App = ({ children }) =>
     <div>
     	{ children }
         <Header />
