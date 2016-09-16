2ea752f879383861410f6c1553379a0bf3738c94
Jhoel <jhoel.quispe.123@gmail.com>
2016-09-16 10:05:45 -0500

    header logo component

---
+++ src/components/atoms/HeaderLogoImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import deltaBankImage from '../../images/deltaBankImage.png';
+
+const HeaderLogoImage = () => (
+  <a href="#" className="hidden-xs">
+    <img src={deltaBankImage}></img>
+  </a>
+)
+
+export default HeaderLogoImage;
