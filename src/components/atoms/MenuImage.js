a31c1bca65c8d3df7ff397f5f1d3c4d0f02ddb79
Jhoel <jhoel.quispe.123@gmail.com>
2016-09-16 10:09:00 -0500

    menu component with static content

---
+++ src/components/atoms/MenuImage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import menuImage from '../../images/menuImage.png';
+
+const MenuImage = () => (
+    <img src={menuImage} className="menu-image"></img>
+)
+
+export default MenuImage;
---
+++ src/components/templates/container/Menu.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import MenuImage from '../../atoms/MenuImage'
+
+//Get from API
+const ContentMenuComponent = () => (
+  <ul className='nav'>
+    <li className="nav-section">
+      <div className="nav-section--header">
+        CONSULTAS
+      </div>
+      <hr/>
+      <ul className="">
+        <li>Posición</li>
+        <li>Estado de Cuenta</li>
+      </ul>
+    </li>
+    <hr/>
+    <li className="nav-section">
+      <div className="nav-section--header">
+        TRANSFERENCIAS
+      </div>
+      <hr/>
+      <ul className="nav-section--links">
+        <li>Entre cuentas</li>
+      </ul>
+      <ul className="nav-section--links">
+        <li>A terceros</li>
+      </ul>
+    </li>
+    <hr/>
+  </ul>
+)
+
+class Menu extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      NavVisible: false
+    };
+  }
+
+  renderMenuBar() {
+    if(this.state.NavVisible) {
+      return  <ContentMenuComponent />;
+    }
+  }
+
+  handleMenuClick() {
+    if(!this.state.NavVisible) {
+      this.setState({NavVisible: true});
+    } else {
+      this.setState({NavVisible: false});
+    }
+  }
+
+  render(){
+    return (
+      <span>
+        <a href="#" className="menu-image" onClick={() => this.handleMenuClick()}>
+          <MenuImage />
+        </a>
+        {this.renderMenuBar()}
+      </span>
+    )
+  }
+}
+
+export default Menu;
