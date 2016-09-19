00c7ca19695379fcbd60096da49a5782eb0237c8
william <wcubasalas@gmail.com>
2016-09-19 17:24:24 -0500

    delete unnecesary markup add route login-password

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route , IndexRoute } from 'react-router';
 
 import LoginUser from './components/templates/login/LoginUser';
+import LoginPassword from './components/templates/login/LoginPassword';
 import Transference1 from './components/templates/transferences/Transference1.js';
 
 //components
@@ -11,4 +12,5 @@ export const getRoutes = () =>
   <Route path="/" component={App}>
     <IndexRoute component={LoginUser}/>
     <Route path="transference1" component={Transference1}/>
+    <Route path="login-password" component={LoginPassword}/>
   </Route>
