69ef1bd1c5dd1ca5ba44d4ca680a913969977178
Rojazefi <zegarraronald@gmail.com>
2016-09-20 14:20:21 -0500

    adding transfer2 to routes

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import App from './containers/App'
 import LoginUser from './components/templates/login/LoginUser';
 import LoginPassword from './components/templates/login/LoginPassword';
 import Transference1 from './components/templates/transferences/Transference1.js';
+import Transference2 from './components/templates/transferences/Transference2.js';
 import Transference3 from './components/templates/transferences/Transference3.js';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={LoginUser}/>
     <Route path="transference3" component={Transference3}/>
+    <Route path="transference2" component={Transference2}/>
     <Route path="transference1" component={Transference1}/>
     <Route path="login-password" component={LoginPassword}/>
   </Route>
