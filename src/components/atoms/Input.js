a5e933fc949ccd08333342017b3696f54540221d
Martin Varela <martinvarela92@icloud.com>
2016-09-20 10:03:10 -0500

    refctor input.js

--- src/components/atoms/Input.js
+++ src/components/atoms/Input.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Input =({ placeholder,id,type }) =>
-  <input
-    className="form-control" 
-    id={id}
-    type ={type} 
-    placeholder={placeholder}
+const Input = ({ placeholder, id, type }) =>
+  <input 
+  className="form-control" 
+  id={id} 
+  type={type} 
+  placeholder={placeholder} 
   />
 
 export default Input;
