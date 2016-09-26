3a40d92c6b65996ac5b6ebdc1d92d728811759a1
Jhoel <jhoel.quispe.123@gmail.com>
2016-09-26 10:47:41 -0500

    add language in loginUser

--- src/components/templates/login/LoginUser.js
+++ src/components/templates/login/LoginUser.js
@@ -3,40 +3,50 @@ import LoginLogo from '../../atoms/loginAtoms/Logo'
 import HelpText from  '../../atoms/loginAtoms/HelpText'
 import InputUser from '../../atoms/loginAtoms/InputUser'
 import Button from '../../atoms/Button'
+import { defineMessages, injectIntl, intlShape, FormattedMessage } from 'react-intl';
 
-const LoginUser = () => (
-  <div>
-    <div className="l-vertical-horizontal-center">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <LoginLogo />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <form className="login-username l-center">
-              <div className="form-group login-border clearfix">
-                <i className="fa fa-user fa-2x l-left" >
-                </i>
-                <div className="l-left">
-                  <InputUser className="login-input" placeholder="NOMBRE DE USUARIO"/>
-                </div>
+class LoginUser extends React.Component  {
+
+  render() {
+    return (
+      <div>
+        <div className="l-vertical-horizontal-center">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12">
+                <LoginLogo />
               </div>
-              <Button link="/login-password" className="login-button" text="SIGUIENTE"/>
-            </form>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 center-block box--footer-message">
-            <HelpText text="Le invitamos a ingresar a nuestra página web para mantenerse actualizado en todo lo referente a las medidas de seguridad básicas, que debe tomaren cuenta para mantener su "
-              aditional_text="información segura"
-            />
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <form className="login-username l-center">
+                  <div className="form-group login-border clearfix">
+                    <i className="fa fa-user fa-2x l-left" >
+                    </i>
+                    <div className="l-left">
+                      <InputUser className="login-input" placeholder={this.context.intl.formatMessage({id:"USER_NAME"})}/>
+                    </div>
+                  </div>
+                  <Button link="/login-password" className="login-button" text={this.context.intl.formatMessage({id:"NEXT"})}/>
+                </form>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12 center-block box--footer-message">
+                <HelpText text="Le invitamos a ingresar a nuestra página web para mantenerse actualizado en todo lo referente a las medidas de seguridad básicas, que debe tomaren cuenta para mantener su "
+                  aditional_text="información segura"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-)
+    );
+  }
+}
+
+LoginUser.contextTypes ={
+ intl:React.PropTypes.object.isRequired
+}
 
 export default LoginUser;
